16ff9d299fb02bae2ea91cca28cd9408d6f1be54
Akhil <akhilu5042@gmail.com>
2022-02-12 06:18:28 +0530

    UC-3_AddNewContact

--- AdvAddressBookSystem/Program.cs
+++ AdvAddressBookSystem/Program.cs
@@ -16,12 +16,30 @@ namespace AddressBookADO.Net
 
             // addressBookManagement.DataBaseConnection(); //UC1
 
-            addressBookManagement.GetAllContact(); //UC1
+            //addressBookManagement.GetAllContact(); //UC1
 
 
 
 
             Console.ReadLine();
+        }
+        /* UC3:- Ability to insert new Contacts to Address Book */
+        public static void AddNewContactDetails()
+        {
+            AddressBookManagement repository = new AddressBookManagement();
+            AddressBookModel model = new AddressBookModel();
+            model.FirstName = "Akhil";
+            model.LastName = "Umnebadi";
+            model.Address = "Sirugapuram";
+            model.City = "Kurnool";
+            model.State = "Andhra Pradesh";
+            model.Zip = "518348";
+            model.PhoneNumber = "1234567891";
+            model.EmailId = "akhil@gmail.com";
+            model.AddressBookType = "Friend";
+            model.AddressBookName = "UmnebadiAkhil";
+
+            Console.WriteLine(repository.AddDataToTable(model) ? "Record inserted successfully\n" : "Record inserted Failed");
         }
     }
 }
